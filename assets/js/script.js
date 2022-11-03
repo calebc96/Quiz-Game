712be8f631321b6550be6a9b85337b2d8aa34a61
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2022-11-03 14:03:00 -0400

    Working on displaying the first object/question in the correct format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,42 @@
 var startButton = document.getElementById("start-btn");
-var firstQuestion = document.getElementById("front-page")
-
+var questionBox = document.getElementById("front-page");
+var timer = document.getElementById("keeping-time");
 
 var questions = [
    {
     title: 'Commonly used data types DO not include:',
     choices: ['strings', 'booleans', 'alerts', 'numbers'],
-    Answers: 'alerts',
+    answers: 'alerts',
    },
    {
-    title: 'The condition in an if / else statement is enclosed with______.',
+    title: 'Commonly used data types DO not include:',
     choices: ['strings', 'booleans', 'alerts', 'numbers'],
-    Answers: 'alerts',
+    answers: 'alerts',
    },
-   {
-    title: 'arrays in JavaScript can be used to store______.',
-    choices: ['strings', 'booleans', 'alerts', 'numbers'],
-    Answers: 'alerts',
-   },
-   {
-    title: 'String values must be enclosed within_______when being assigned to variables.',
-    choices: ['strings', 'booleans', 'alerts', 'numbers'],
-    Answers: 'alerts',
-   },
-   {
-    title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
-    choices: ['strings', 'booleans', 'alerts', 'numbers'],
-    Answers: 'alerts',
-   },
+  
 ]
-var listEl = document.createElement("ol");
 
-listE1[]
+var secondsLeft = 100;
 
-var li1 = document.createElement("li");
-var li2 = document.createElement("li");
-var li3 = document.createElement("li");
-var li4 = document.createElement("li");
-
-li1.textContent = "Strings";
-li2.textContent = "Booleans";
-li3.textContent = "alerts";
-li4.textContent = "numbers";
-
-listEl.appendChild(li1);
-listEl.appendChild(li2);
-listEl.appendChild(li3);
-listEl.appendChild(li4);
-
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timer.textContent = secondsLeft;
+      
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+      }
+    }, 1000)
+  }
 
 startButton.addEventListener("click", function() {
-    firstQuestion.textContent = questionOne
-    firstQuestion.appendChild(listEl);
-
+    startquiz();
+    setTime();
 });
 
+
+function startquiz () { 
+    var myArray = JSON.stringify(questions);
+    document.getElementById("front-page").textContent = myArray;
+}
